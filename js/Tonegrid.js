4adf9bac7e5d162b73827d5f400c0141c15d97d0
Soma Holiday <soma@somaholiday.com>
2015-10-17 17:51:31 +0200

    Add event handlers to play Gibber sounds

--- js/Tonegrid.js
+++ js/Tonegrid.js
@@ -3,11 +3,45 @@ var frame = 0;
 animate();
 
 function animate() {
-  // move stuff here
+    // move stuff here
 
-  frame++;
-  requestAnimationFrame(animate);
+    frame++;
+    requestAnimationFrame(animate);
 }
+
+function initGibber() {
+    Gibber.init();
+    s = Synth({
+        attack: ms(10),
+        decay: ms(50),
+        maxVoices: 4
+    });
+}
+
+$(function() {
+
+    initGibber();
+
+    var objTag = document.getElementById('cellObject');
+    objTag.addEventListener('load', function() {
+
+        var svgDoc = objTag.getSVGDocument();
+
+        svgDoc.onmouseover = function(event) {
+            var targetId = event.target.id;
+
+            console.log("Target: \t" + targetId);
+
+            if (targetId.charAt(0) == 'i') {
+                var note = parseInt(targetId.substr(1));
+                console.log("Note: \t\t" + note);
+                s.decay = ms(rndi(50,1000));
+                s.note(note);
+
+            }
+        }
+    });
+});
 
 /*
    // Display framerate
@@ -16,4 +50,3 @@ function animate() {
    frame = 0;
    }, 1000);
 */
-
