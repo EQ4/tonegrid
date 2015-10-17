b028bee0d2d4acdd22f66a44d93fb049540f73cb
Abe Pazos <abe@hamoid.com>
2015-10-17 16:35:45 +0200

    Remove pixi, add jquery

--- js/Tonegrid.js
+++ js/Tonegrid.js
@@ -1,61 +1,16 @@
-
-var renderer = new PIXI.WebGLRenderer(600, 600, {
-  antialias: true
-});
-
-document.body.appendChild(renderer.view);
-
-var stage = new PIXI.Container();
-
-var sz = 2;
-var totalSprites = 20000;
-var sprites = new PIXI.ParticleContainer(totalSprites, {
-  position: true,
-  alpha: true
-});
-
-stage.addChild(sprites);
-
-var maggots = [];
 var frame = 0;
-
-var gfx = new PIXI.Graphics();
-gfx.lineStyle(0);
-gfx.beginFill(0xFFFFFF);
-gfx.drawCircle(15, 15, 1.5);
-gfx.endFill();
-
-var texture = gfx.generateTexture(); 
-
-for(var i=0; i<totalSprites; i++) {
-  var dude = new PIXI.Sprite(texture);
-  dude.anchor.set(0.5);
-  dude.x = 100;
-  dude.y = 100;
-  dude.dist = Math.random();
-  dude.alpha = 0.3;
-  maggots.push(dude);
-  sprites.addChild(dude);
-}
 
 animate();
 
 function animate() {
-  for (var i = 0; i < totalSprites; i++) {
-    var dude = maggots[i];
-
-    dude.x += Math.random() - 0.5;
-    dude.y += Math.random() - 0.5;
-  }
+  // move stuff here
 
   frame++;
-
-  renderer.render(stage);
-
   requestAnimationFrame(animate);
 }
 
 /*
+   // Display framerate
    var iv = setInterval(function() {
    console.log(frame);
    frame = 0;
